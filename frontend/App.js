5a7f49503ab19636fb504cc9f16e21ebde60c57e
Sean Sullivan <sean_sullivan@college.harvard.edu>
2019-01-15 21:43:39 +0100

    Frontend?

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  static getDerivedStateFromProps() {
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  onChange = text => {
+    this.setState({ text });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.label}>Username</Text>
+        <TextInput placeholder="Username" onChangeText={this.onChange} value={this.state.text} />
+        <Text style={styles.label}>Password</Text>
+        <TextInput placeholder="Password" onChangeText={this.onChange} value={this.state.text} />
+        <TouchableOpacity style={styles.button}>
+          <Text>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,8 +43,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginHorizontal: 20,
     justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  button: {
+    backgroundColor: '#dddddd',
+    alignSelf: 'center',
+    padding: 10,
+    paddingHorizontal: 15,
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+  }
 });
